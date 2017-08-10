4a672c6401969b337b8087487b1ab36868564c43
Shyam Gupta <postboxshyam@gmail.com>
2017-08-10 15:54:32 -0400

    Commit

--- server.py
+++ server.py
@@ -6,11 +6,11 @@ def print_scores():
     a_dictionary = {'John':45,'Mary':90,'Beth':78}
     return render_template('index.html',scores=a_dictionary)
 
-app.run(port=5000,debug=True)
-
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add_integers():
     number1=request.args.get('num1')
     number2=request.args.get('num2')
     return render_template('add.html',number1,number2)
+
+app.run(port=5000,debug=True)
