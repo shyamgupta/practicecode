c856819b41d80ab9b3588c99068c757b627f4ac2
Shyam Gupta <postboxshyam@gmail.com>
2017-08-11 11:24:10 -0400

    Commit

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
-from flask import Flask,render_template,request
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/scores')
-def print_scores():
-    a_dictionary = {'John':45,'Mary':90,'Beth':78}
-    return render_template('index.html',scores=a_dictionary)
-
-
-@app.route('/add/<int:num1>/<int:num2>')
-def add_integers(num1,num2):
-    number1=request.args.get('num1')
-    number2=request.args.get('num2')
-    return render_template('add.html',number1=num1,number2=num2)
+@app.route('/')
+def hello():
+    my_name = request.args.get('name')
+    my_color = request.args.get('color')
+    return '{} {}'.format(my_name,my_color)
 
 app.run(port=5000,debug=True)
